033fdac3b94f10d71d450fb8f952780d3dffd7bb
True-Beast ‎ <singhsubm24@gamil.com>
2025-03-09 17:04:19 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
-import Play from './components/play'
+import Play from './components/Play'
 
 function App() {
   const [start, setStart] = useState(false)
